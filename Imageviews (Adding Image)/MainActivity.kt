8374474e9c1392a9358666f0e963cc5b7da1ae84
Brett <148084194+hoybrett99@users.noreply.github.com>
2024-08-28 19:54:57 +0100

    Add files via upload

---
+++ Imageviews (Adding Image)/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication2
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Button to add an image
+        binding.btnAddImage.setOnClickListener {
+
+            // Finding the image from the resource folder
+            binding.ivImage.setImageResource(R.drawable.meerkat)
+        }
+    }
+}
